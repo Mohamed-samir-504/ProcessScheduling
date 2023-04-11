da207c443aac10e6200613a1192e3b5354b64d28
markkashraf <mark2001rko2001@gmail.com>
2023-04-11 23:32:35 +0200

    initial commit

--- src/main/java/com/collegegroup/processscheduling/HelloApplication.java
+++ src/main/java/com/collegegroup/processscheduling/HelloApplication.java
@@ -2,22 +2,35 @@ package com.collegegroup.processscheduling;
 
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
+import javafx.scene.Group;
+import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.image.Image;
+import javafx.scene.paint.Color;
 import javafx.stage.Stage;
 
 import java.io.IOException;
 
 public class HelloApplication extends Application {
+
+    //Scene : a drawing surface for graphical content (Text, shapes , images)
+
+
     @Override
     public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 320, 240);
-        stage.setTitle("Hello!");
-        stage.setScene(scene);
-        stage.show();
-    }
+        try {
 
+            Parent root = FXMLLoader.load(getClass().getResource("Scene1.fxml"));
+            Scene scene = new Scene(root);
+            stage.setScene(scene);
+            stage.show();
+
+        } catch(Exception e) {
+            e.printStackTrace();
+        }
+    }
     public static void main(String[] args) {
         launch();
     }
+
 }
--- src/main/java/com/collegegroup/processscheduling/Process.java
+++ src/main/java/com/collegegroup/processscheduling/Process.java
@@ -4,4 +4,13 @@ public class Process {
     private int pid;
     private int burst;
     private int arrivalTime;
+
+    public Process(int pid, int burst, int arrivalTime) {
+        this.pid = pid;
+        this.burst = burst;
+        this.arrivalTime = arrivalTime;
+    }
+    public Process()
+    {}
+
 }
